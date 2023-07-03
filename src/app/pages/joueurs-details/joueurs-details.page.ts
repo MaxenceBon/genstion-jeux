ffd5933fffc77bde5f27e7f521e7ba534d231fad
Maxence Bon <maxence.bon@orange.fr>
2023-07-03 14:10:41 +0200

    Ajout camera

--- src/app/pages/joueurs-details/joueurs-details.page.ts
+++ src/app/pages/joueurs-details/joueurs-details.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Joueur, JoueurService } from 'src/app/service/joueur.service';
 
 @Component({
@@ -15,8 +16,20 @@ export class JoueursDetailsPage implements OnInit {
   joueur!: Joueur;
   editedJoueur!: Joueur;
   private joueurService: JoueurService = inject(JoueurService);
+  avatarImage!: string;
   
   constructor() { }
+
+  async uploadPhoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Prompt,
+    });
+  
+    this.avatarImage = `data:image/jpeg;base64,${image.base64String}`;
+  }
 
   ngOnInit() {
     this.joueur = this.joueurService.getCurrentJoueur();
